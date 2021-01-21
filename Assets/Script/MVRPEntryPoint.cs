e87de4d781391835d53fc8de46224186089d8400
Sugi249 <66174315+Sugi249@users.noreply.github.com>
2021-01-21 19:58:27 +0900

    スクリプト１

---
+++ Assets/Script/MVRPEntryPoint.cs
@@ -0,0 +1,22 @@
+﻿us  
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MVRPEntryPoint : MonoBehaviour
+{
+    [SerializeField]
+    private MVRPInput Input;
+    [SerializeField]
+    private MVRPView View;
+
+    void Start()
+    {
+        MVRPModel model = new MVRPModel();
+
+        Input.InjectModel(model);
+
+        MVRPPresenter presenter = new MVRPPresenter(model, View);
+    }
+
+}
