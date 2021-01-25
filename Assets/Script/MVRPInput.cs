5d6ea2d42f349bcd35005c809cf12df7550b58ad
tcagame <tcagame@tca.ac.jp>
2021-01-25 11:11:08 +0900

    C

---
+++ Assets/Script/MVRPInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class MVRPInput : MonoBehaviour
+{
+    [SerializeField]
+    private Button UpButton;
+    [SerializeField]
+    private Button DownButton;
+    [SerializeField]
+    private Button LeftButton;
+    [SerializeField]
+    private Button RightButton;
+
+    private MVRPModel Model;
+
+    public void InjectModel(MVRPModel model)
+    {
+        Model = model;
+    }
+
+    void Start()
+    {
+        UpButton.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                Vector3 position = Model.CubePosition.Value;
+                position.z += 3f;
+                Model.CubePosition.Value = position;
+            });
+        DownButton.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                Vector3 position = Model.CubePosition.Value;
+                position.z -= 3f;
+                Model.CubePosition.Value = position;
+            });
+
+
+
+    }
+}
